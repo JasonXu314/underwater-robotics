d3f47ee29a25c656bf2f5149ed9e7c72a8291951
Jason Xu <Xinwen31415@gmail.com>
2022-02-06 00:30:06 -0600

    added href matching

--- replace.js
+++ replace.js
@@ -1,18 +1,21 @@
 function findByText(root, text) {
-	const children = Array.from(root.children),
+	const children = Array.from(root.childNodes),
 		out = [];
+
+	if (root.href && root.href.includes(text)) {
+		out.push({ elem: root, attr: 'href' });
+	}
 	if (children.length > 0) {
 		children.forEach((child) => out.push(...findByText(child, text)));
 	} else if (root.textContent.includes(text)) {
-		out.push(root);
+		out.push({ elem: root, attr: 'textContent' });
 	}
 	return out;
 }
 
 for (const tag in globalNames) {
 	const hasName = findByText(document.body, tag);
-	console.log(hasName);
-	hasName.forEach((elem) => {
-		elem.textContent = elem.textContent.replaceAll(tag, globalNames[tag]);
+	hasName.forEach(({ elem, attr }) => {
+		elem[attr] = elem[attr].replaceAll(tag, globalNames[tag]);
 	});
 }
